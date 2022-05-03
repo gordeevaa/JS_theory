8f1edf4fa280116c9b9d2acf5af059210105eeb8
Alena Gordeeva <gordeeva.aln@gmail.com>
2022-05-03 20:15:21 +0500

    7 TOPIC: Cycles and labels.

--- js/script.js
+++ js/script.js
@@ -325,3 +325,59 @@ for (let i = 1; i < 10; i++) {
 
     console.log(i);
 }
+
+/* 7 ТЕМА: Цикл в цикле и метки.  */
+
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+    for (let j = 0; j < 3; j++) {
+        console.log(j);       
+    }
+}
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+let result = '';
+const length = 7;
+
+for (let i = 1; i < length; i++) {
+
+    for (let j = 0; j < i; j++) {
+        result += "*";     
+    }
+
+    result += '\n';
+}
+
+console.log(result);
+
+
+/* МЕТКИ. */
+
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`);
+        for (let k = 0; k < 3; k++) {
+            if (k === 2) continue first;
+            console.log(`Third level: ${k}`);      
+        }       
+    }
+}
+
+
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`);
+        for (let k = 0; k < 3; k++) {
+            if (k === 2) break first;
+            console.log(`Third level: ${k}`);      
+        }       
+    }
+}
